3225601069b9477fa61e6029e76f3b607c35f737
sourobrata.sarkar <sourobrata.sarkar@mountblue.io>
2019-03-28 18:28:55 +0530

    FEAT: Add endpoints to retrieve, update and delete a movie

--- src/server/server.js
+++ src/server/server.js
@@ -8,6 +8,8 @@ const { Movie } = require("../models/movie");
 const { Customer } = require("../models/customer");
 const { Rental } = require("../models/rental");
 
+app.use(bodyParser.json());
+
 app.get("/", async (req, res) => {
   res.status(200).send("sent");
 });
@@ -27,10 +29,10 @@ app.get("/api/movies", async (req, res) => {
 
 //add a movie
 app.post("/api/movies", (req, res) => {
-  const title = req.query.title;
-  const genre = req.query.genre;
-  const numberInStock = req.query.numberInStock;
-  const dailyRentalRate = req.query.dailyRentalRate;
+  const title = req.body.title;
+  const genre = req.body.genre;
+  const numberInStock = req.body.numberInStock;
+  const dailyRentalRate = req.body.dailyRentalRate;
   const newMovie = new Movie({
     title: title,
     genre: genre,
@@ -38,6 +40,42 @@ app.post("/api/movies", (req, res) => {
     dailyRentalRate: dailyRentalRate
   });
   newMovie.save().then(response => {
+    res.status(200).send(response);
+  });
+});
+
+//retrieve a movie based on id
+app.get("/api/movies/:movieId", async (req, res) => {
+  const movieId = req.params.movieId;
+  await Movie.findOne({ _id: movieId }).then(response => {
+    res.status(200).send(response);
+  });
+});
+
+//update the details of a movie
+app.put("/api/movies/:movieId", async (req, res) => {
+  const movieId = req.params.movieId;
+  const title = req.body.title;
+  const genre = req.body.genre;
+  const numberInStock = req.body.numberInStock;
+  const dailyRentalRate = req.body.dailyRentalRate;
+
+  let movie = {
+    title,
+    genre,
+    numberInStock,
+    dailyRentalRate
+  };
+
+  await Movie.findOneAndUpdate({ _id: movieId }, { $set: movie }, { new: true }).then(response => {
+    res.status(200).send(response);
+  });
+});
+
+//delete a movie
+app.delete("/api/movies/:movieId", async (req, res) => {
+  const movieId = req.params.movieId;
+  await Movie.findOneAndDelete({ _id: movieId }).then(response => {
     res.status(200).send(response);
   });
 });
